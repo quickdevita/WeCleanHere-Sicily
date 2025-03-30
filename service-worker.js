04bde307be04306f9be52129e859eac5a17edb21
quickdevita <quickdevita@gmail.com>
2025-03-30 20:44:33 +0200

    Prima versione online

---
+++ service-worker.js
@@ -0,0 +1,21 @@
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open('wecleansicily-cache').then((cache) => {
+            return cache.addAll([
+                '/',
+                'index.html',
+                'assets/styles/style.css',
+                'js/main.js',
+                'manifest.json'
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((cachedResponse) => {
+            return cachedResponse || fetch(event.request);
+        })
+    );
+});
